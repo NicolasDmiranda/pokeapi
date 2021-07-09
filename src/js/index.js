1e4cd63135626c0f58a403c98f96382bfc3dd2ca
nicolas miranda <nicolasformal415@gmail.com>
2021-07-08 21:28:01 -0400

    pokeapi final

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,23 @@
-let api ="https://pokeapi.co/api/v2/";
+let api ="https://pokeapi.co/api/v2/"; // here I return the values ​​to 0 when cleaning
 window.onload = function(){
     //buscar---------------------------------------------------
     var element = window.document.getElementById("buscarv")
-    element.addEventListener("click", buscar);
+    element.addEventListener("click", buscar); // here i make the click event call me the search function
 
     //limpiar---------------------------------------------------
     var element2 = window.document.getElementById("limpiar")
-    element2.addEventListener("click", limpiar);
+    element2.addEventListener("click", limpiar); // here i make the click event call me the clean function
  }
 
  function limpiar(){
     document.getElementById("textoBuscar").value = "";
     var detalle = window.document.getElementById("detalle")
-    detalle.classList.add("d-none")
+    detalle.classList.add("d-none") // here i return the values ​​to 0 when cleaning
  }
 
 function buscar() {
     var name = document.getElementById("textoBuscar").value.toLowerCase();
-     axios.get(api + 'pokemon/' + name) //https://pokeapi.co/api/v2/pokemon/{name}/
+     axios.get(api + 'pokemon/' + name) //https://pokeapi.co/api/v2/pokemon/{name}/ -> this is how my url would look
         .then(function (response)
         {     
             procesardatos(response.data);
@@ -26,12 +26,12 @@ function buscar() {
         {  
             var detalle = window.document.getElementById("detalle")
             detalle.classList.add("d-none")
-            alert("Tu pokemon ingresado no se encuentra")
+            alert("El pokemon ingresado no se encuentra")
             console.log(error);
         })
 }
 
-function procesardatos(data){
+function procesardatos(data){ // this is where i am processing all the data in the api
 
     //img---------------------------------------------------
     var imagen = window.document.getElementById("imagen");
@@ -41,7 +41,7 @@ function procesardatos(data){
     var nombre = window.document.getElementById("nombre");
     nombre.innerText = data.name;
 
-     //tipo---------------------------------------------------
+     //type---------------------------------------------------
      var tipo = window.document.getElementById("tipo");
      tipo.innerText = data.types[0].type.name;
 
@@ -60,14 +60,15 @@ function procesardatos(data){
     var movimientos = window.document.getElementById("movimientos");
     movimientos.innerHTML="";
     var listaMovimientos = data.moves;
-
-    for (let i = 3; i >= 0 ; i--) {
+    
+    for (let i = 3; i >= 0 ; i--) { 
         const element = listaMovimientos[i];
         var li = document.createElement("li");
         li.innerText = element.move.name;
         movimientos.append(li);
+        //  in the for I made it count the first 4 movements of the list in the api
     }
 
     var detalle = window.document.getElementById("detalle")
-    detalle.classList.remove("d-none")
+    detalle.classList.remove("d-none") 
 }
